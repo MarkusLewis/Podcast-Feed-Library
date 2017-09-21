44d5a81240fb723f03568b904ea42539ef818501
DanKottke <dkottke@npr.org>
2017-09-21 14:20:46 -0400

    added bom protection

--- src/main/java/com/icosillion/podengine/models/Podcast.java
+++ src/main/java/com/icosillion/podengine/models/Podcast.java
@@ -10,6 +10,8 @@ import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
+import net.pempek.unicode.UnicodeBOMInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -42,15 +44,18 @@ public class Podcast {
     public Podcast(URL feed) throws InvalidFeedException, MalformedFeedException {
         URLConnection ic = null;
         InputStream is = null;
+        UnicodeBOMInputStream ubis = null;
 
         try {
             ic = feed.openConnection();
             ic.setRequestProperty("User-Agent", "PodEngine/2.0");
             is = ic.getInputStream();
+            ubis = new UnicodeBOMInputStream(is);
+            ubis.skipBOM();
 
             this.feedURL = feed;
             this.resolvedURL = ic.getURL();
-            this.xmlData = IOUtils.toString(is);
+            this.xmlData = IOUtils.toString(ubis);
             this.document = DocumentHelper.parseText(xmlData);
             this.rootElement = this.document.getRootElement();
             this.channelElement = this.rootElement.element("channel");
@@ -62,6 +67,7 @@ public class Podcast {
         } catch (DocumentException e) {
             throw new InvalidFeedException("Error parsing feed XML.", e);
         } finally {
+            IOUtils.closeQuietly(ubis);
             IOUtils.closeQuietly(is);
         }
     }
