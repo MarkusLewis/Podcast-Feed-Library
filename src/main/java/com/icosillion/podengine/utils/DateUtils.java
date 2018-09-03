ebf7d67dcc378cfce5d05bd598825c81b4a7e954
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2018-09-03 12:12:22 +0200

    Simplify.

--- src/main/java/com/icosillion/podengine/utils/DateUtils.java
+++ src/main/java/com/icosillion/podengine/utils/DateUtils.java
@@ -8,16 +8,6 @@ import java.util.Locale;
 public class DateUtils {
 
     public static Date stringToDate(String dt) {
-        Date result = parseWithStandardDateFormats(dt);
-
-        if (result == null) {
-            return parseWithStandardDateFormats(normalize(dt));
-        } else {
-            return result;
-        }
-    }
-
-    private static Date parseWithStandardDateFormats(String dt) {
         SimpleDateFormat[] dateFormats = {
                 new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US),
                 new SimpleDateFormat("dd MMM yyyy HH:mm:ss Z", Locale.US),
@@ -25,11 +15,13 @@ public class DateUtils {
                 new SimpleDateFormat("dd MMM yyyy HH:mm Z", Locale.US)
         };
 
+        String normalizedDt = normalize(dt);
+
         Date date = null;
 
         for (SimpleDateFormat dateFormat : dateFormats) {
             try {
-                date = dateFormat.parse(dt);
+                date = dateFormat.parse(normalizedDt);
                 break;
             } catch (ParseException e) {
                 //This format didn't work, keep going
