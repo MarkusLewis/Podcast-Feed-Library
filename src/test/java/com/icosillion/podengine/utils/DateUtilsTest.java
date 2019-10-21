922b9923f76b60d9be4afc011e9c09e35924bcd2
Marcus Lewis <marcus@icosillion.com>
2019-10-21 19:46:34 +0100

    Fix DateUtils tests

--- src/test/java/com/icosillion/podengine/utils/DateUtilsTest.java
+++ src/test/java/com/icosillion/podengine/utils/DateUtilsTest.java
@@ -2,12 +2,14 @@ package com.icosillion.podengine.utils;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class DateUtilsTest {
 
@@ -20,19 +22,29 @@ public class DateUtilsTest {
         Date date = DateUtils.stringToDate(dateTime);
 
         assertNotNull(date);
-        assertEquals("Tue, 03 Mar 2009 16:00:00 +0100", sdf.format(date));
+        assertEquals("Tue, 03 Mar 2009 15:00:00 +0000", sdf.format(date));
     }
 
     @Test
     public void parsesNonStandardDateFormats() {
-        assertEquals("Tue, 03 Mar 2009 16:00:00 +0100",
-                     sdf.format(DateUtils.stringToDate("Tues, 03 March 2009 15:00:00 -0000")));
+        try {
+            assertEquals(
+                    sdf.parse("Tue, 03 Mar 2009 16:00:00 +0100"),
+                    DateUtils.stringToDate("Tues, 03 March 2009 15:00:00 -0000")
+            );
 
-        assertEquals("Wed, 04 Mar 2009 16:00:00 +0100",
-                     sdf.format(DateUtils.stringToDate("Wednes, 04 March 2009 15:00:00 -0000")));
+            assertEquals(
+                    sdf.parse("Wed, 04 Mar 2009 16:00:00 +0100"),
+                    DateUtils.stringToDate("Wednes, 04 March 2009 15:00:00 -0000")
+            );
 
-        assertEquals("Thu, 05 Mar 2009 16:00:00 +0100",
-                     sdf.format(DateUtils.stringToDate("Thurs, 05 March 2009 15:00:00 -0000")));
+            assertEquals(
+                    sdf.parse("Thu, 05 Mar 2009 16:00:00 +0100"),
+                    DateUtils.stringToDate("Thurs, 05 March 2009 15:00:00 -0000")
+            );
+        } catch (ParseException ex) {
+            fail("Failed to parse date: " + ex.getMessage());
+        }
     }
 
 }
